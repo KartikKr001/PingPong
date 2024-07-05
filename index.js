d81cb16811e37ee5427257eff6da4583751e5420
Kartik Kumar <kartikkrprajapati01@gmail.com>
2024-07-05 08:40:17 +0530

    Paddle hits ball

--- index.js
+++ index.js
@@ -13,7 +13,13 @@ document.addEventListener('DOMContentLoaded',()=>{
     
     let dx = 2;  // displacement in x direction
     let dy = 2;  // displacement in y direction
+    let Py = 5;  // displacement in x direction of paddle
     setInterval(function exec(){
+        // ball hits paddle
+        if(ballY >= Py && ballY <= Py+paddle.offsetHeight && ballX <= paddle.style.left+paddle.offsetWidth){
+            dx*=-1;
+        }
+        
         ballX += dx;        
         ball.style.left = `${ballX}px`;
         if(ballX > table.offsetWidth-ball.offsetWidth || ballX < 0) dx*=-1;  //CHANGING DIRECTIONS
@@ -21,12 +27,13 @@ document.addEventListener('DOMContentLoaded',()=>{
         ballY += dy;        
         ball.style.top = `${ballY}px`;
         if(ballY > table.offsetHeight-ball.offsetHeight || ballY <= 0) dy*=-1;  //CHANGING DIRECTION
+
     },5);
 
 
-    let Py = 5;  // displacement in x direction
 
     document.addEventListener('keydown',(event)=>{
+        event.preventDefault(); //prevents default event behaviour , for unwantesd scrolling up-down of page
         if(event.key == 'ArrowUp'){
             Py = Math.max(0,Py-40);
             paddle.style.top = `${Py}px`
